1bab68513faa671d5de5980d169ca20d7073ef46
Enes <Enes@DESKTOP-CRDL160>
2023-06-01 17:51:59 +0300

    Proje dosyası ekle.

---
+++ AsyncPortScanner.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AsyncPortScanner
+{
+    partial class AsyncPortScanner
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipAddressTextBox = new System.Windows.Forms.TextBox();
+            this.startPortTextBox = new System.Windows.Forms.TextBox();
+            this.endPortTextBox = new System.Windows.Forms.TextBox();
+            this.ScanButton = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ipAddressTextBox
+            // 
+            this.ipAddressTextBox.Location = new System.Drawing.Point(12, 34);
+            this.ipAddressTextBox.Name = "ipAddressTextBox";
+            this.ipAddressTextBox.Size = new System.Drawing.Size(121, 22);
+            this.ipAddressTextBox.TabIndex = 0;
+            // 
+            // startPortTextBox
+            // 
+            this.startPortTextBox.Location = new System.Drawing.Point(12, 103);
+            this.startPortTextBox.Name = "startPortTextBox";
+            this.startPortTextBox.Size = new System.Drawing.Size(121, 22);
+            this.startPortTextBox.TabIndex = 1;
+            // 
+            // endPortTextBox
+            // 
+            this.endPortTextBox.Location = new System.Drawing.Point(12, 178);
+            this.endPortTextBox.Name = "endPortTextBox";
+            this.endPortTextBox.Size = new System.Drawing.Size(121, 22);
+            this.endPortTextBox.TabIndex = 2;
+            // 
+            // ScanButton
+            // 
+            this.ScanButton.Location = new System.Drawing.Point(12, 228);
+            this.ScanButton.Name = "ScanButton";
+            this.ScanButton.Size = new System.Drawing.Size(121, 34);
+            this.ScanButton.TabIndex = 3;
+            this.ScanButton.Text = "Scan";
+            this.ScanButton.UseVisualStyleBackColor = true;
+            this.ScanButton.Click += new System.EventHandler(this.ScanButton_Click);
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.ItemHeight = 16;
+            this.resultListBox.Location = new System.Drawing.Point(247, 34);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(124, 228);
+            this.resultListBox.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 303);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(359, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ip Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Start Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "End Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Open Ports";
+            // 
+            // AsyncPortScanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 352);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.ScanButton);
+            this.Controls.Add(this.endPortTextBox);
+            this.Controls.Add(this.startPortTextBox);
+            this.Controls.Add(this.ipAddressTextBox);
+            this.Name = "AsyncPortScanner";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.AsyncPortScanner_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipAddressTextBox;
+        private System.Windows.Forms.TextBox startPortTextBox;
+        private System.Windows.Forms.TextBox endPortTextBox;
+        private System.Windows.Forms.Button ScanButton;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AsyncPortScanner.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AsyncPortScanner
+{
+    public partial class AsyncPortScanner : Form
+    {
+        public AsyncPortScanner()
+        {
+            InitializeComponent();
+        }
+
+        private async void ScanButton_Click(object sender, EventArgs e)
+        {
+            string ipAddress = ipAddressTextBox.Text;
+            int startPort = Convert.ToInt32(startPortTextBox.Text);
+            int endPort = Convert.ToInt32(endPortTextBox.Text);
+
+            progressBar.Minimum = startPort;
+            progressBar.Maximum = endPort;
+            progressBar.Value = startPort;
+            progressBar.Visible = true;
+
+            await ScanOpenPortsAsync(ipAddress, startPort, endPort);
+
+
+        }
+
+        private async Task ScanOpenPortsAsync(string ipAddress, int startPort, int endPort)
+        {
+            List<int> openPorts = new List<int>();
+
+            for (int port = startPort; port <= endPort; port++)
+            {
+                TcpClient tcpClient = new TcpClient();
+
+                try
+                {
+                    await tcpClient.ConnectAsync(ipAddress, port);
+                    openPorts.Add(port);
+                    tcpClient.Close();
+                }
+                catch (SocketException)
+                {
+
+                }
+
+                progressBar.Value = port;
+            }
+
+            UpdateListBox(openPorts);
+        }
+
+        private void UpdateListBox(List<int> openPorts)
+        {
+            if (openPorts.Count > 0)
+            {
+                resultListBox.Items.Clear();
+                foreach (int port in openPorts)
+                {
+                    resultListBox.Items.Add(port.ToString());
+                }
+            }
+            else
+            {
+                resultListBox.Items.Clear();
+                resultListBox.Items.Add("No open ports found.");
+            }
+        }
+
+        private void AsyncPortScanner_Load(object sender, EventArgs e)
+        {
+            
+                this.AutoSize = true;
+                this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+    }
+}
